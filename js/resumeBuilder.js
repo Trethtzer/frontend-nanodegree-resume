4eaad91d35ef7623618162c8032a3a8f543e9588
Guillermo Siles Bonilla <joker.crimson@gmail.com>
2017-06-04 22:58:07 +0200

    Validating JSON
    Pos eso

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,62 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Guillermo Siles Bonilla")
+
+var formattedName = HTMLheaderName.replace("%data%","Guillermo Siles Bonilla");
+var formattedRole = HTMLheaderRole.replace("%data%","Software Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+
+var biography = {
+	"name" : "Guillermo Siles Bonilla",
+	"role" : "Software Developer",
+	"contact info" : {
+		"email" : "gsilesbonilla@gmail.com",
+		"github" : "trethzer",
+		"location" : "Malaga",
+		"mobile" : "619388053"
+	},
+	"picture URL" : "images/fry.jpg",
+	"welcome message" : "Bienvenido a mi pagina web",
+	"skills" : ["JavaScript", "Robotics", "RPA", "BluePrism"]
+};
+
+var work = {};
+work.position = "unemployed";
+work.years_worked = "0";
+work.status = "Currently looking for a job";
+
+var education = {
+	"schools" : [
+		{
+			"name": "University of Malaga",
+			"city": "Malaga",
+			"degree": "Computer Science",
+			"years": 5
+		}
+	]
+}
+
+$("#main").append(work.position);
+$("#main").append(education.last_school);
+
+
+
+
+
+
+
+
+
+
+
+// FUNCIONES
+
+function formatName(nombre){
+	initialLetter = nombre.slice(0,1).toUpperCase();
+	resto = nombre.slice(1).toLowerCase();
+	composed = initialLetter + resto;
+	return composed;
+}
