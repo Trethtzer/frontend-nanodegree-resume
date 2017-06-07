735d1a50e4ceb7b5f3a007254c7fdced6ec62cb4
Guillermo Siles Bonilla <joker.crimson@gmail.com>
2017-06-07 09:12:54 +0200

    Lesson 3
    lesson 3 completed.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,30 +15,84 @@ var biography = {
 	"role" : "Software Developer",
 	"contact info" : {
 		"email" : "gsilesbonilla@gmail.com",
-		"github" : "trethzer",
-		"location" : "Malaga",
-		"mobile" : "619388053"
+		"github" : "https://www.github.com/Trethtzer",
+		"location" : "Málaga, Spain",
+		"mobile" : "(+34) 619 38 80 53",
+		"linkedin" : "https://www.linkedin.com/in/guillermosilesbonilla/"
 	},
 	"picture URL" : "images/fry.jpg",
-	"welcome message" : "Bienvenido a mi pagina web",
-	"skills" : ["JavaScript", "Robotics", "RPA", "BluePrism"]
+	"welcome message" : "Software Developer with over 4+ years of experience in Java. Possess a Degree in Computer Science. Main areas are Android development, robotics and computer vision."
 };
 
-var work = {};
-work.position = "unemployed";
-work.years_worked = "0";
-work.status = "Currently looking for a job";
 
-var education = {
-	"schools" : [
+var skills = {
+	"languages" : [
 		{
-			"name": "University of Malaga",
-			"city": "Malaga",
-			"degree": "Computer Science",
-			"years": 5
+			"name" : "Spanish",
+			"level" : "Native speaker"
+		},
+		{
+			"name" : "English",
+			"level" : "B2 Cambridge, 88 TOEFL"
+		},
+		{
+			"name" : "Korean",
+			"level" : "A2"
 		}
-	]
-}
+	],
+	"computer_languages" : ["Java","Android","JavaScript","html5","Css","C#","C","SQL","Haskell","Latex","Matlab","C++","Python","R"],
+	"others" : ["Eclipse","AndroidStudio","Github","Linux","OpenCV","Excel"]
+};
+
+
+var work = {};
+work.position = "Software developer";
+work.company = "ViewNext";
+work.years_worked = "0";
+
+var education = [
+	{
+		"name" : "Computer science degree",
+		"school" : "University of Malaga",
+		"start_date" : "September 2012",
+		"end_date" : "December 2016"
+	},
+	{
+		"name" : "Development of applications for Android devices, in collaboration with Samsung",
+		"school" : "University of Malaga",
+		"start_date" : "2016",
+		"end_date" : "2016"
+	},
+	{
+		"name" : "Android developer nanodegree",
+		"school" : "Udacity",
+		"start_date" : "November 2016",
+		"end_date" : "June 2016"
+	}
+];
+
+var project = [
+	{
+		"name" : "Automatic recording of a game of Go through computer vision",
+		"start_date" : "June 2016",
+		"end_date" : "February 2017",
+		"notes" : [
+					"Android app that automatically identifies and record moves played during a go game.",
+					"Developed using a variety of computer vision techniques.",
+					"App can record an entire Go game between two players lasting 1 – 2 hours, depending on the battery quality; Received final mark: 9.3 / 10."
+				]
+	},
+	{
+		"name" : "Go Tournament held in Malaga",
+		"start_date" : "2012",
+		"end_date" : "2012",
+		"notes" : [
+			"Managed Spanish Go Tournament in Málaga.",
+			"Helped all participants to solve their problems and organized activities in the city of Málaga.",
+			"Received positives reviews from participants."
+		]
+	}
+];
 
 $("#main").append(work.position);
 $("#main").append(education.last_school);
