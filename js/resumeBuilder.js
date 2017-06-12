26b46121b048f43146722b3ed4a38c257578e6fb
Guillermo Siles Bonilla <joker.crimson@gmail.com>
2017-06-12 09:18:52 +0200

    Lesson 5
    Finished and extra added.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -85,12 +85,12 @@ var project = [
 					"Android app that automatically identifies and record moves played during a go game.",
 					"Developed using a variety of computer vision techniques.",
 					"App can record an entire Go game between two players lasting 1 – 2 hours, depending on the battery quality; Received final mark: 9.3 / 10."
-				]
+				],
 	},
 	{
 		"name" : "Go Tournament held in Malaga",
 		"start_date" : "2012",
-		"end_date" : "2012",
+		"end_date" : "",
 		"notes" : [
 			"Managed Spanish Go Tournament in Málaga.",
 			"Helped all participants to solve their problems and organized activities in the city of Málaga.",
@@ -99,8 +99,7 @@ var project = [
 	}
 ];
 
-$("#main").append(work.position);
-$("#main").append(education.last_school);
+var API = "AIzaSyChaxQBVNPTFWEX-4UM2fYdf04xUtA_kRI";
 
 
 
@@ -121,4 +120,45 @@ function formatName(nombre){
 	resto = nombre.slice(1).toLowerCase();
 	composed = initialLetter + resto;
 	return composed;
+}
+
+function displayWork(){
+	for(index in work){
+      $("#workExperience").append(HTMLworkStart);
+      $(".work-entry:last").append(HTMLworkEmployer.replace('%data%',work[index].company) + HTMLworkTitle.replace('%data%',work[index].position));
+      if(work[index].end_date === ""){
+        $(".work-entry:last").append(HTMLworkDates.replace('%data%', work[index].start_date));
+      }
+      else{
+        $(".work-entry:last").append(HTMLworkDates.replace('%data%', work[index].start_date + " - " + work[index].end_date));
+      }
+      $(".work-entry:last").append(HTMLworkDescription.replace('%data%',"I developed robotics software, working on projects for banks and other big companies. Also I got certifications for the Blueprism tool and C#."));
+    }
+}
+
+function displayProjects(){
+	for(index in project){
+      $("#projects").append(HTMLprojectStart);
+      $(".project-entry:last").append(HTMLprojectTitle.replace('%data%',project[index].name));
+      if(project[index].end_date === ""){
+        $(".project-entry:last").append(HTMLprojectDates.replace('%data%', project[index].start_date));
+      }
+      else{
+        $(".project-entry:last").append(HTMLprojectDates.replace('%data%', project[index].start_date + " - " + project[index].end_date));
+      }
+      $(".project-entry:last").append(HTMLprojectDescription.replace('%data%',degloseProject(project[index].notes)));
+    }
+}
+function degloseProject(array){
+	var result = "";
+	for(index in array){
+		result = result + array[index] + "<br>";
+	}
+	return result;
+}
+
+function inName(nombre){
+	var splitted = nombre.split(' ');
+	var formatedName = splitted[0] + " " + splitted[1].toUpperCase();
+	return formatedName;
 }
